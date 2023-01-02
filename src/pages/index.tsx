4e9d9a0e5af8a3eafeae0912dd5f8fc88ee16dd5
hiroaki-saito <hiro021312@gmail.com>
2023-01-02 21:30:36 +0900

    [mod]index.tsxの不要なposition削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { Box, Heading } from "@chakra-ui/layout";
 export default function Home(): JSX.Element {
   return (
     <>
-      <Box position={"relative"} minHeight={"100vh"}>
+      <Box minHeight={"100vh"}>
         <Header></Header>
         <Heading size="lg" color={"black"} paddingLeft={"4rem"} flexGrow={15}>
           1on1ログ
